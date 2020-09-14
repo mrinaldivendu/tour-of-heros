10f31cc3073d190bc7d275bdcbb5a44da4df4c56
unknown <mrinaldivendu@gmail.com>
2020-09-14 23:20:33 +0530

    Tour of Heros

---
+++ src/app/mock.heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROS: hero[] = [
+  { id : 11, name : 'Wolverine' },
+  { id : 12, name: 'Flash' },
+  { id: 13, name: 'BatMan' },
+  { id: 14, name: 'Deadpool' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'SpiderMan' },
+  { id: 17, name: 'vision' },
+  { id: 18, name: 'Thor' },
+  { id: 19, name: 'Captain' },
+  { id: 20, name: 'Iron man' }
+];
